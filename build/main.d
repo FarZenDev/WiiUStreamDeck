1b18d825e203737c14a4f6a5eefb71ed026fc44e
FarZenDev <mathissalban3@gmail.com>
2026-03-02 00:41:48 +0100

    Create main.d

---
+++ build/main.d
@@ -0,0 +1,169 @@
+main.o: E:/WiiUStreamDeck/source/main.cpp \
+ C:/devkitPro/wut/include/whb/proc.h C:/devkitPro/wut/include/wut.h \
+ C:/devkitPro/wut/include/wut_structsize.h \
+ C:/devkitPro/wut/include/wut_types.h \
+ C:/devkitPro/wut/include/wut_rplwrap.h \
+ C:/devkitPro/wut/include/whb/log.h \
+ C:/devkitPro/wut/include/whb/log_udp.h \
+ C:/devkitPro/wut/include/vpad/input.h \
+ C:/devkitPro/wut/include/vpadbase/base.h \
+ C:/devkitPro/wut/include/coreinit/thread.h \
+ C:/devkitPro/wut/include/coreinit/alarm.h \
+ C:/devkitPro/wut/include/coreinit/context.h \
+ C:/devkitPro/wut/include/coreinit/threadqueue.h \
+ C:/devkitPro/wut/include/coreinit/time.h \
+ C:/devkitPro/wut/include/coreinit/systeminfo.h \
+ C:/devkitPro/wut/include/coreinit/exception.h \
+ C:/devkitPro/wut/include/coreinit/mutex.h \
+ C:/devkitPro/wut/include/coreinit/cache.h \
+ C:/devkitPro/wut/include/coreinit/screen.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_main.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_stdinc.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_config.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_platform.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/begin_code.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/close_code.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_assert.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_atomic.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_audio.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_error.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_endian.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_mutex.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_thread.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_rwops.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_clipboard.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_cpuinfo.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_events.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_video.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_pixels.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_rect.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_surface.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_blendmode.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_keyboard.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_keycode.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_scancode.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_mouse.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_joystick.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_guid.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_gamecontroller.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_sensor.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_quit.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_gesture.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_touch.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_filesystem.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_haptic.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_hidapi.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_hints.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_loadso.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_log.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_messagebox.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_metal.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_power.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_render.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_shape.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_system.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_timer.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_version.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_locale.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_misc.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_ttf.h \
+ C:/devkitPro/portlibs/wiiu/include/SDL2/SDL.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/curl.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/curlver.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/system.h \
+ C:/devkitPro/wut/include/sys/socket.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/easy.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/multi.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/curl.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/urlapi.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/options.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/header.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/websockets.h \
+ C:/devkitPro/portlibs/wiiu/include/curl/mprintf.h \
+ E:/WiiUStreamDeck/source/json.hpp E:/WiiUStreamDeck/source/font.h
+C:/devkitPro/wut/include/whb/proc.h:
+C:/devkitPro/wut/include/wut.h:
+C:/devkitPro/wut/include/wut_structsize.h:
+C:/devkitPro/wut/include/wut_types.h:
+C:/devkitPro/wut/include/wut_rplwrap.h:
+C:/devkitPro/wut/include/whb/log.h:
+C:/devkitPro/wut/include/whb/log_udp.h:
+C:/devkitPro/wut/include/vpad/input.h:
+C:/devkitPro/wut/include/vpadbase/base.h:
+C:/devkitPro/wut/include/coreinit/thread.h:
+C:/devkitPro/wut/include/coreinit/alarm.h:
+C:/devkitPro/wut/include/coreinit/context.h:
+C:/devkitPro/wut/include/coreinit/threadqueue.h:
+C:/devkitPro/wut/include/coreinit/time.h:
+C:/devkitPro/wut/include/coreinit/systeminfo.h:
+C:/devkitPro/wut/include/coreinit/exception.h:
+C:/devkitPro/wut/include/coreinit/mutex.h:
+C:/devkitPro/wut/include/coreinit/cache.h:
+C:/devkitPro/wut/include/coreinit/screen.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_main.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_stdinc.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_config.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_platform.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/begin_code.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/close_code.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_assert.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_atomic.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_audio.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_error.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_endian.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_mutex.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_thread.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_rwops.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_clipboard.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_cpuinfo.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_events.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_video.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_pixels.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_rect.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_surface.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_blendmode.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_keyboard.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_keycode.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_scancode.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_mouse.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_joystick.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_guid.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_gamecontroller.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_sensor.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_quit.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_gesture.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_touch.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_filesystem.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_haptic.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_hidapi.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_hints.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_loadso.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_log.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_messagebox.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_metal.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_power.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_render.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_shape.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_system.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_timer.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_version.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_locale.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_misc.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL_ttf.h:
+C:/devkitPro/portlibs/wiiu/include/SDL2/SDL.h:
+C:/devkitPro/portlibs/wiiu/include/curl/curl.h:
+C:/devkitPro/portlibs/wiiu/include/curl/curlver.h:
+C:/devkitPro/portlibs/wiiu/include/curl/system.h:
+C:/devkitPro/wut/include/sys/socket.h:
+C:/devkitPro/portlibs/wiiu/include/curl/easy.h:
+C:/devkitPro/portlibs/wiiu/include/curl/multi.h:
+C:/devkitPro/portlibs/wiiu/include/curl/curl.h:
+C:/devkitPro/portlibs/wiiu/include/curl/urlapi.h:
+C:/devkitPro/portlibs/wiiu/include/curl/options.h:
+C:/devkitPro/portlibs/wiiu/include/curl/header.h:
+C:/devkitPro/portlibs/wiiu/include/curl/websockets.h:
+C:/devkitPro/portlibs/wiiu/include/curl/mprintf.h:
+E:/WiiUStreamDeck/source/json.hpp:
+E:/WiiUStreamDeck/source/font.h:
